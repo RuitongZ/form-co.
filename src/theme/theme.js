856e9e3d15f81a432a00bf2947fdb09c21fdf6b2
CaesarHp <ryllazhu@gmail.com>
2024-06-27 11:11:18 -0400

    Theme updates

---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins, san-serif',
+    h2: {
+      fontFamily: 'Font1, Brogadier, serif',
+    },
+    body1: {
+      fontFamily: 'Poppins, san-serif',
+    },
+    body2: {
+      fontFamily: 'Font2, Cera Pro, san-serif',
+    },
+  },
+});
+
+export default theme;
