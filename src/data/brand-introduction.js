63ce8bbe2d7a4d8cfdeed8ab17d63e64216f8a20
CaesarHp <ryllazhu@gmail.com>
2024-08-06 13:34:27 -0400

    data changes

---
+++ src/data/brand-introduction.js
@@ -0,0 +1,32 @@
+import TRADITION from '../images/tradition.png';
+import FERMLIVING from '../images/ferm-living.png';
+import LEMNOS from '../images/lemnos.png';
+import HAY from '../images/hay.png';
+
+export const BRAND_INTRODUCTION = [
+  {
+    brand: '&Tradition',
+    introduction: `Founded in 2010 in Copenhagen, &Tradition is an international brand celebrated for its Danish craftsmanship and design legacy. The company combines innovation with tradition, offering both new designs and reissued classics. Collaboration is key at &Tradition, involving employees, designers, and partners in shaping the brand’s future, fostering a creative and respectful environment.
+    Central to &Tradition’s mission are sustainability and ethical practices. The brand is dedicated to making responsible choices that benefit both the planet and its people. Beyond creating exceptional products, &Tradition aims to build a legacy of design excellence and responsible craftsmanship for future generations.`,
+    img: TRADITION,
+  },
+  {
+    brand: 'Ferm Living',
+    introduction: `Life’s contrasts drive the search for balance and self-expression, leading people to seek spaces that reflect their true selves and offer comfort. The brand focuses on authentic design and responsible choices to create products and environments that achieve this balance.
+    Based in Copenhagen, the brand collaborates with global artisans, merging Scandinavian sensibilities with various skills and traditions. Their collections, characterized by soft forms, rich textures, and unique details, craft composed atmospheres with unexpected elements. Committed to sustainability, the brand ensures that every aspect, from materials to production, supports responsible choices. Their furniture, accessories, and lighting help individuals create spaces where they can genuinely feel at home.`,
+    img: FERMLIVING,
+  },
+  {
+    brand: 'LEMNOS',
+    introduction: `TAKATA Lemnos Inc., founded in 1947 in Takaoka-city, Japan, initially specialized in brass casting and established a key partnership with Seikosha Co., Ltd. (now Seiko Time Creation Inc.) in 1966.
+    In the late 1980s, Lemnos expanded into original design, launching the “Lemnos Brand” in 1989 with Kazuo Kawasaki’s acclaimed “HOLA” clock. Since then, collaborations with renowned designers like Riki Watanabe and Shin Azumi have produced celebrated and distinctive works.
+    Today, Lemnos is esteemed worldwide for its design and interior accessories. The company blends traditional craftsmanship with modern innovation, creating timeless pieces that maintain enduring beauty and relevance.`,
+    img: LEMNOS,
+  },
+  {
+    brand: 'HAY',
+    introduction: `HAY believes that good design should be accessible to all. Since its founding, co-founders Mette and Rolf Hay have partnered with top global designers to create high-quality products for a broad audience. This principle drives the company’s mission.
+    HAY’s designs address the blending of personal and professional spaces, offering versatile furniture, lighting, and accessories that fit various environments. From everyday items like toothbrushes to larger pieces like sofas, HAY provides a fresh perspective on functionality.Inspired by art, architecture, and fashion, HAY ensures their designs are contemporary and relevant. Based in Copenhagen with a global presence, their stores reflect this evolving vision.`,
+    img: HAY,
+  },
+];
