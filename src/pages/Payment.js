9f3a441313a5db014a40226932aea02128947b41
CaesarHp <ryllazhu@gmail.com>
2024-08-09 17:00:10 -0400

    page changes

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,6 +1,12 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { clearCart } from '../redux/cartSlice';
+import { setClickedPage } from '../redux/pagesSlice';
+
 import { Box, Typography, Button } from '@mui/material';
 import CheckoutForm from '../components/CheckoutForm';
 import OrderSummary from '../components/OrderSummary';
+import ThanksCardDialoy from '../components/ThanksCardDialoy';
 
 const bodyContainer = {
   pt: '50px',
@@ -17,6 +23,14 @@ const titleStyles = {
   justifyContent: 'center',
 };
 
+const CheckoutFormBox = {
+  display: 'flex',
+  flexDirection: { xs: 'column-reverse', md: 'row' },
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  gap: '50px',
+};
+
 const payNowButton = {
   mt: '20px',
   backgroundColor: '#000',
@@ -29,7 +43,36 @@ const payNowButton = {
   },
 };
 
+const orderSummayBox = {
+  width: { xs: '100%', lg: '40%' },
+  border: '1px solid #000',
+};
+
 function Payment() {
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+
+  const handleSubmit = () => {
+    setOpen(true);
+  };
+
+  const handelClose = () => {
+    setOpen(false);
+    dispatch(clearCart());
+    window.scroll({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleClickContinue = () => {
+    setOpen(false);
+    dispatch(clearCart());
+    dispatch(setClickedPage(null));
+    window.scroll({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }, []);
+
   return (
     <Box sx={bodyContainer}>
       <Box sx={titleStyles}>
@@ -38,29 +81,28 @@ function Payment() {
         </Typography>
       </Box>
 
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column-reverse', md: 'row' },
-          justifyContent: 'space-between',
-          alignItems: 'flex-start',
-          gap: '50px',
-        }}
-      >
+      <Box sx={CheckoutFormBox}>
         <Box sx={{ width: { xs: '100%', lg: '60%' } }}>
           <CheckoutForm />
 
-          <Button fullWidth disableRipple sx={payNowButton}>
+          <Button
+            fullWidth
+            disableRipple
+            type='submit'
+            onClick={handleSubmit}
+            sx={payNowButton}
+          >
             Pay Now
           </Button>
         </Box>
 
-        <Box
-          sx={{
-            width: { xs: '100%', lg: '40%' },
-            border: '1px solid #000',
-          }}
-        >
+        <ThanksCardDialoy
+          open={open}
+          onClose={handelClose}
+          onClick={handleClickContinue}
+        />
+
+        <Box sx={orderSummayBox}>
           <OrderSummary />
         </Box>
       </Box>
